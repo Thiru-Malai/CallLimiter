fb27f76310f59e4ec7c89c8dbf2649c2616c4679
Thirumalai <malaithiru370@gmail.com>
2025-05-17 15:37:39 +0530

    Uncomment OneTimeWorker

--- app/src/main/java/com/thirumalai/calllimiter/BroadcastReceivers/BootReceiver.java
+++ app/src/main/java/com/thirumalai/calllimiter/BroadcastReceivers/BootReceiver.java
@@ -7,8 +7,13 @@ import android.content.Intent;
 import android.os.Build;
 import android.util.Log;
 
-import com.thirumalai.calllimiter.CallMonitorService;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+
+import com.thirumalai.calllimiter.CallMonitorWorker;
 import com.thirumalai.calllimiter.PreferenceHelper;
+
+import java.util.concurrent.TimeUnit;
 
 public class BootReceiver extends BroadcastReceiver {
     @SuppressLint("UnsafeProtectedBroadcastReceiver")
@@ -19,13 +24,13 @@ public class BootReceiver extends BroadcastReceiver {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             int activeLimitContactSize = PreferenceHelper.getAllContactSize();
             if(activeLimitContactSize > 0){
-//                OneTimeWorkRequest workRequest =
-//                        new OneTimeWorkRequest.Builder(CallMonitorWorker.class)
-//                                .setInitialDelay(10, TimeUnit.SECONDS) // delay avoids crash
-//                                .build();
-//                WorkManager.getInstance(context).enqueue(workRequest);
-                context.startService(new Intent(context, CallMonitorService.class));
-                Log.d("BootReceiver", "Started Foreground Service from Boot Receiver");
+                OneTimeWorkRequest workRequest =
+                        new OneTimeWorkRequest.Builder(CallMonitorWorker.class)
+                                .setInitialDelay(10, TimeUnit.SECONDS) // delay avoids crash
+                                .build();
+                WorkManager.getInstance(context).enqueue(workRequest);
+//                context.startService(new Intent(context, CallMonitorService.class));
+//                Log.d("BootReceiver", "Started Foreground Service from Boot Receiver");
             }
         }
     }
