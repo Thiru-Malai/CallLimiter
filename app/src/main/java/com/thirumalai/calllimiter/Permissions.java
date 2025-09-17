a61e0a7ccf8e162e4d13b0df00f68eda5ed96656
Thirumalai <malaithiru370@gmail.com>
2025-09-17 09:52:33 +0530

    Minor UI Change

--- app/src/main/java/com/thirumalai/calllimiter/Permissions.java
+++ app/src/main/java/com/thirumalai/calllimiter/Permissions.java
@@ -32,11 +32,11 @@ public class Permissions extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_permissions);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.permissions_layout), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.permissions_layout), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
 
         back_btn = findViewById(R.id.back_btn_permissions);
 
